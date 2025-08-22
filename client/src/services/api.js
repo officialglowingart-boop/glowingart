2c9a15243029e423072d00e50a6a8f090c99017c
mr-fahad-03 <mdfahadzulfiqar@gmail.com>
2025-08-22 23:51:06 +0500

    api.js fixed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_BASE_URL = import.meta.env.REACT_APP_API_URL 
+const API_BASE_URL = "https://glowingart-ruddy.vercel.app/api"
 //const API_BASE_URL = "http://localhost:5000/api"
 
 
